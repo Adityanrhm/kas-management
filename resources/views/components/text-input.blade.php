38759b2f25a5c53125b1d4339731d6abbaa220fe
huza <huzaifahasif3@gmail.com>
2025-06-26 21:08:53 +0700

    style(input): fucking autofill styling and prevent nonsense input lock
    - Updated CSS for autofill to ensure consistent background, border, and text color
    - Added readonly and onfocus to prevent input lock after autofill
    - Disabled fucking spellcheck. cause weird nigga text
    - Improved focus and hover states for better UX

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,33 +1,30 @@
 <style>
     @layer components {
-        input:-webkit-autofill {
-            -webkit-box-shadow: 0 0 0 1000px #0D0D0D inset !important;
-            -webkit-text-fill-color: #d1d5db !important;
-            border: none;
-        }
-
-        input:-webkit-autofill:focus {
-            -webkit-box-shadow: 0 0 0 1000px #0D0D0D inset !important;
-            border: none;
-        }
-
         input:-webkit-autofill,
         input:-webkit-autofill:focus,
         input:-webkit-autofill:hover,
         input:-webkit-autofill:active {
-            -webkit-text-fill-color: #d1d5db !important;
+            -webkit-box-shadow: 0 0 0 1000px #0D0D0D inset !important;
+            -webkit-text-fill-color: rgb(255, 255, 255) !important;
             border: 1px solid rgba(255, 255, 255, 0.25) !important;
             border-radius: 0.5rem !important;
             transition: background-color 5000s ease-in-out 0s;
+            caret-color: white;
         }
     }
 </style>
 
 @props(['disabled' => false])
 
-<input @disabled($disabled)
+<input
+    @disabled($disabled)
+    readonly
+    spellcheck="false"
+    onfocus="this.removeAttribute('readonly')"
+    autocomplete="username"
     {{ $attributes->merge([
-        'class' => 'block w-full rounded-lg pl-10 border-[rgba(255,255,255,0.25)] dark:border-[rgba(255,255,255,0.25)] \ 
-            dark:bg-white/5 dark:text-gray-300 focus:border-[rgba(255,255,255,0.25)] dark:focus:border-[rgba(255,255,255,0.25)] \ 
-            focus:ring-[rgba(255,255,255,0.25)] ',
-    ]) }}>
+        'class' => 'block w-full rounded-lg pl-10 border-[rgba(255,255,255,0.25)] dark:border-[rgba(255,255,255,0.25)] dark:bg-white/5 \
+            dark:text-gray-300 focus:border-[rgba(255,255,255,0.25)] dark:focus:border-[rgba(255,255,255,0.25)] focus:ring-[rgba(255,255,255,0.25)] \
+                shadow-sm text-sm',
+    ]) }}
+>
