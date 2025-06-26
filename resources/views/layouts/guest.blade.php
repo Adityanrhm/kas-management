a83056c0ca81b4703147b4f411ff3c262a82d3dd
huza <huzaifahasif3@gmail.com>
2025-06-26 21:38:27 +0700

    style(logo): add white drop shadow to application logo
    - Added custom class `.wds` with drop-shadow filter
    - Logo now has a kalcer white glow effect

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,11 +22,11 @@
         @include('partials.preloader')
         <div>
             <a href="/">
-                <x-application-logo class="w-24 h-24 fill-current text-gray-500" />
+<x-application-logo class="w-24 h-24 fill-current text-gray-500 wds" />
             </a>
         </div>
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
+            class="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg ">
             {{ $slot }}
         </div>
     </div>
