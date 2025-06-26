0d10409f7142689d50fc2c8e2d4ab8092dd87a12
huza <huzaifahasif3@gmail.com>
2025-06-26 20:08:26 +0700

    style(button): implement white glow effect when button is pressed
    - Added custom CSS for box-shadow glow on button active state
    - Button now emits white glow when pressed

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,6 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge([
+    'type' => 'submit',
+    'class' => 'inline-flex items-center px-4 py-2 bg-white text-gray-800 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest hover:bg-gray-100 focus:bg-white/10 focus:backdrop-blur-md focus:border-white/40 focus:border-2 focus:text-white active:bg-white/10 active:backdrop-blur-md active:border-white/40 active:border active:text-white transition ease-in-out duration-250 btn-active-glow'
+]) }}>
     {{ $slot }}
 </button>
