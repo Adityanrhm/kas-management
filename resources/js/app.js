c80956ade70f17b0c16758552018cf126ba62016
huza <huzaifahasif3@gmail.com>
2025-06-27 16:27:39 +0700

    Fix Alpine.js persist plugin loading
    - Import and initialize @alpinejs/persist before Alpine.start()
    - Fixes issue with  not found

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@ import "./bootstrap";
 
 import Alpine from "alpinejs";
 import "@fortawesome/fontawesome-free/js/all.js";
+import persist from '@alpinejs/persist'
 
+Alpine.plugin(persist)
 window.Alpine = Alpine;
 
 Alpine.start();
