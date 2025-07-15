063ae2c96d318a0a9b848946c73a0d4e02070b9d
huza <huzaifahasif3@gmail.com>
2025-07-15 22:16:09 +0700

    feat: implement AOS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import "./bootstrap";
-
 import Alpine from "alpinejs";
 import persist from "@alpinejs/persist";
 import "@fortawesome/fontawesome-free/js/all.js";
 import Swal from "sweetalert2";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 Alpine.plugin(persist);
 window.Alpine = Alpine;
 window.Swal = Swal;
 
+AOS.init();
 Alpine.start();
